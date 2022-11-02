831b3de0d3b689a23abc2d7ea34d4945b302d91a
PeteChicchetti <pj0524@gmail.com>
2022-11-01 20:53:42 -0400

    schema and seed tested and working

--- db/seed.sql
+++ db/seed.sql
@@ -3,13 +3,17 @@ VALUES  ('Service'),
         ('Installation'),
         ('Human Resources');
 
-INSERT INTO roles (title, salary)
-VALUES  ('Technician', 55000),
-        ('Installer', 40000),
-        ('HR Admin', 65000);
+INSERT INTO roles (title, salary, department_id)
+VALUES  ('Lead Tech', 60000, 1),
+        ('Technician', 50000, 1),
+        ('Lead Installer', 45000, 2),
+        ('Installer', 37000, 2),
+        ('HR Admin', 65000, 3);
 
 
-INSERT INTO employee (first_name, last_name)
-VALUES  ('Pete', 'Chicchetti'),
-        ('John', 'Smith'),
-        ('Chistopher', 'Lee');
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('Pete', 'Chicchetti', 1, NULL),
+        ('John', 'Smith', 2, 1),
+        ('Chistopher', 'Lee', 3, NULL),
+        ('Elliot', 'Smith', 4, 3),
+        ('Verónica', 'Rodriguez', 5, NULL);
