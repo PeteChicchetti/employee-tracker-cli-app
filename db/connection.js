87c8fb5560ff0110a7ed27935c77499adae9a274
PeteChicchetti <pj0524@gmail.com>
2022-11-01 21:07:02 -0400

    added connection to node

--- db/connection.js
+++ db/connection.js
@@ -8,3 +8,25 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'rootroot',
+      database: 'business_db'
+    },
+    console.log(`Connected to the business_db database.`)
+  );
+  
+  // Default response for any other request (Not Found)
+  app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
