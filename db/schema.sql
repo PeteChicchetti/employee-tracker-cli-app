fda6a93d2f0623f215a4d4577a2cc25c30294c54
PeteChicchetti <pj0524@gmail.com>
2022-11-01 20:04:32 -0400

    changed structure of schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,30 +4,24 @@ CREATE DATABASE business_db;
 USE business_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
 );
 
 CREATE TABLE roles (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT NOT NULL,
-  FOREIGN KEY (department_id)
-  REFERENCES department(id),
-  PRIMARY KEY (id)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-  id INT AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT NOT NULL,
-  FOREIGN KEY (role_id)
-  REFERENCES roles(id),
-  FOREIGN KEY (manager_id)
-  REFERENCES employee(id),
-  PRIMARY KEY (id)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
---
+++ db/seed.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (name)
+VALUES  ('Service'),
+        ('Installation'),
+        ('Human Resources');
+
+INSERT INTO roles (title, salary)
+VALUES  ('Technician', 55000),
+        ('Installer', 40000),
+        ('HR Admin', 65000);
+
+
+INSERT INTO employee (first_name, last_name)
+VALUES  ('Pete', 'Chicchetti'),
+        ('John', 'Smith'),
+        ('Chistopher', 'Lee');
